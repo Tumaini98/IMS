9d87edd59a7bafdaff8b7635ae22d42fc2c85ea5
Tumaini98 <ujoshuatumaini@gmail.com>
2024-02-15 21:25:56 -0600

    added prducts

--- IMS.CoreBusiness/Product.cs
+++ IMS.CoreBusiness/Product.cs
@@ -7,21 +7,20 @@ using System.Threading.Tasks;
 
 namespace IMS.CoreBusiness
 {
-    public class Inventory
+    public class Product
     {
-        public int InventoryId { get; set; }
+        public int ProductId { get; set; }
 
 
         [Required]
         [StringLength(150)]
-        public string InventoryName { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
 
-
-        [Range(0, int.MaxValue, ErrorMessage ="Quantity must be greater or equal to 0")]
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greater or equal to 0")]
         public int Quantity { get; set; }
 
-
         [Range(0, int.MaxValue, ErrorMessage = "Price must be greater or equal to 0")]
-        public double  Price { get; set; }
+        public double Price { get; set; }
+
     }
 }
